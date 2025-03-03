1e4feae7f8265a352bdab93f03ab363649e68652
Prasad Shelke <99793793+Prasad1102@users.noreply.github.com>
2025-03-03 18:08:26 +0530

    Added order Item artwork functionality refs: SP-63 (#21)
    * Added order Item artwork functionality refs: SP-63
    
    * Refactored Order Item Detail Modal
    
    * user and admin message management completed
    
    * Set alert message for image upload, message send functionality
    
    * some changes from code review
    
    ---------
    
    Co-authored-by: Ganesh Navale <navaleganesh2011@gmail.com>

--- src/components/order_management/OrderItemModal.jsx
+++ src/components/order_management/OrderItemModal.jsx
@@ -7,28 +7,24 @@ import DialogActions from '@mui/material/DialogActions';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 import Grid from '@mui/material/Grid2';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import CancelIcon from '@mui/icons-material/Cancel';
-import LocalShippingIcon from '@mui/icons-material/LocalShipping';
-import InfoIcon from '@mui/icons-material/Info';
+import ImageUploader from '../../utils/ImageUploader';
+import Notification from '../../utils/notification';
 
 import {
-  Avatar,
   Backdrop,
-  Box,
   Card,
   CardContent,
-  Chip,
   CircularProgress,
-  Container,
-  Divider,
   Typography,
 } from '@mui/material';
 import * as API from '../../utils/adminApi';
+import DesignDiscussion from './DesignDiscussion';
 
 const OrderItemModal = (props) => {
   const { itemId, open, onClose } = props;
   const [state, setState] = useState({});
+  const [errors, setErrors] = useState({});
+  const [alert, setAlert] = useState({ message: '', type: '' });
 
   useEffect(() => {
     fetchOrderItem(itemId);
@@ -38,7 +34,6 @@ const OrderItemModal = (props) => {
     try {
       setOpenBackdrop(true);
       const response = await API.fetchOrderItem(itemId);
-      console.log('order', response.data);
       setState(response.data);
     } catch (error) {
       console.log('Failed to fetch Order Item.', error);
@@ -53,68 +48,53 @@ const OrderItemModal = (props) => {
 
   const [openBackdrop, setOpenBackdrop] = useState(false);
 
-  const renderStatusChip = (status) => {
-    switch (status) {
-      case 'Delivered':
-        return (
-          <Chip
-            label="Delivered"
-            color="success"
-            icon={<CheckCircleIcon />}
-            size="small"
-          />
-        );
-      case 'Pending':
-        return (
-          <Chip
-            label="Pending"
-            color="warning"
-            icon={<LocalShippingIcon />}
-            size="small"
-          />
-        );
-      case 'Canceled':
-        return (
-          <Chip
-            label="Canceled"
-            color="error"
-            icon={<CancelIcon />}
-            size="small"
-          />
-        );
-      case 'Not Received':
-        return (
-          <Chip
-            label="Not Received"
-            color="info"
-            icon={<InfoIcon />}
-            size="small"
-          />
-        );
-      case 'Confirmed':
-        return (
-          <Chip
-            label="Confirmed"
-            color="info"
-            icon={<CheckCircleIcon />}
-            size="small"
-          />
-        );
-      case 'Paid':
-        return (
-          <Chip
-            label="paid"
-            color="success"
-            icon={<CheckCircleIcon />}
-            size="small"
-          />
-        );
-      case 'Created':
-        return (
-          <Chip label="Created" color="info" icon={<InfoIcon />} size="small" />
-        );
-      default:
-        return null;
+  const handleImageChange = (image) => {
+    if (image) {
+      setState((prevState) => ({ ...prevState, image: image }));
+      let newErrors = { ...errors };
+      delete newErrors.image;
+      setErrors(newErrors);
+    }
+  };
+
+  const handleFinalArtworkChange = (image) => {
+    if (image) {
+      setState((prevState) => ({ ...prevState, final_artwork: image }));
+      let newErrors = { ...errors };
+      delete newErrors.image;
+      setErrors(newErrors);
+    }
+  };
+
+  const handleSubmit = async () => {
+    if (!state.final_artwork && !state.image) {
+      setAlert({ message: 'No File Selected', type: 'error' });
+      return;
+    }
+
+    try {
+      setOpenBackdrop(true);
+      const formData = new FormData();
+      if (state.final_artwork) {
+        formData.append('order_item[final_artwork]', state.final_artwork);
+      }
+      if (state.image) {
+        formData.append('order_item[image]', state.image);
+      }
+      const response = await API.updateOrderItem(state.id, formData);
+      setState((prevState) => ({
+        ...prevState,
+        final_artwork_url:
+          response.data.final_artwork_url || prevState.final_artwork_url,
+        image_url: response.data.image_url || prevState.image_url,
+        final_artwork: null,
+        image: null,
+      }));
+      setAlert({ message: 'File Uploaded Successfully', type: 'success' });
+    } catch (error) {
+      console.error('Failed to update final artwork:', error);
+    } finally {
+      setOpenBackdrop(false);
     }
   };
 
@@ -126,9 +106,10 @@ const OrderItemModal = (props) => {
       maxWidth="md"
       fullWidth
     >
-      <DialogTitle sx={{}} id="new_user">
-        Order Item Details
-      </DialogTitle>
+      <Grid item>
+        <Notification alert={alert} setAlert={setAlert} />
+      </Grid>
+      <DialogTitle id="new_user">Order Item #{state?.id}</DialogTitle>
       <IconButton
         aria-label="close"
         onClick={handleClose}
@@ -142,30 +123,104 @@ const OrderItemModal = (props) => {
         <CloseIcon />
       </IconButton>
       <DialogContent dividers>
-        <Container sx={{ pt: 5, pb: 5 }}>
-          <Typography variant="h4" gutterBottom fontWeight="bold">
-            Order Item Details
-          </Typography>
-          <Grid container spacing={4}>
-            {/* Order Summary Section */}
-            <Grid item size={{ xs: 12, sm: 12, md: 8 }}>
-              <Card sx={{ mb: 3 }}>
-                <CardContent></CardContent>
-              </Card>
-            </Grid>
-
-            {/* Payment and Order Status Section */}
-            <Grid item size={{ xs: 12, sm: 12, md: 4 }}>
-              <Card sx={{ mb: 3 }}>
-                <CardContent></CardContent>
-              </Card>
-
-              <Card>
-                <CardContent></CardContent>
-              </Card>
-            </Grid>
+        <Grid container spacing={4}>
+          <Grid item size={{ xs: 12, sm: 4, md: 4.5 }}>
+            <Card>
+              <CardContent sx={{ padding: '0 16px !important' }}>
+                <Grid item py={1}>
+                  <Typography variant="h6" gutterBottom fontWeight="bold">
+                    Initial Artwork
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <ImageUploader
+                    height={175}
+                    id="initial-artwork"
+                    imageUrl={state?.image_url}
+                    showDeleteButton={false}
+                    onImageChange={handleImageChange}
+                  />
+                </Grid>
+                <Grid item py={1}>
+                  <Typography variant="h6" gutterBottom fontWeight="bold">
+                    Final Artwork
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <ImageUploader
+                    id="final-artwork"
+                    height={175}
+                    imageUrl={state?.final_artwork_url}
+                    onImageChange={handleFinalArtworkChange}
+                    showDeleteButton={false}
+                    error={!!errors.final_artwork}
+                    helperText={errors.final_artwork}
+                  />
+                </Grid>
+                <Grid item py={1}>
+                  <DialogActions>
+                    <Grid sx={{ padding: '2px 0 0 0' }}>
+                      <Button
+                        variant="outlined"
+                        onClick={() => handleSubmit()}
+                        disabled={openBackdrop}
+                      >
+                        Submit
+                      </Button>
+                    </Grid>
+                  </DialogActions>
+                </Grid>
+              </CardContent>
+            </Card>
           </Grid>
-        </Container>
+          <Grid item size={{ xs: 12, sm: 8, md: 7.5 }}>
+            <Card>
+              <CardContent>
+                <Grid
+                  container
+                  spacing={4}
+                  direction="row"
+                  sx={{
+                    justifyContent: 'space-between',
+                    alignItems: 'top',
+                  }}
+                >
+                  <Grid item size={6}>
+                    <Typography variant="body1" gutterBottom>
+                      <strong>User Name: </strong>
+                      {state?.customer_name}
+                    </Typography>
+                    <Typography variant="body1" gutterBottom>
+                      <strong>Price: </strong>
+                      {state?.price}
+                    </Typography>
+                    <Typography variant="body1" gutterBottom>
+                      <strong>Quantity: </strong>
+                      {state?.quantity}
+                    </Typography>
+                  </Grid>
+                  <Grid item size={6}>
+                    <Typography variant="body1" gutterBottom>
+                      <Typography variant="body1" gutterBottom>
+                        <strong>Item Name: </strong> {state?.product_name}
+                      </Typography>
+
+                      <Typography variant="body1" gutterBottom>
+                        <strong>Order ID: </strong>
+                        {state?.order_id}
+                      </Typography>
+                      <Typography variant="body1" gutterBottom>
+                        <strong>Dimension: </strong>
+                        {state?.height} * {state?.width}
+                      </Typography>
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </Card>
+            <DesignDiscussion itemId={state?.id} />
+          </Grid>
+        </Grid>
       </DialogContent>
       <Backdrop
         sx={(theme) => ({ color: '#fff', zIndex: theme.zIndex.drawer + 1 })}
